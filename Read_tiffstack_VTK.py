03de3c09b638166a6849838be1c7c61a1beaa0e1
BastianJuelich <76682194+BastianJuelich@users.noreply.github.com>
2020-12-29 10:17:41 +0100

    Update Read_tiffstack_VTK.py
    Simple Example

--- Read_tiffstack_VTK.py
+++ Read_tiffstack_VTK.py
@@ -1,19 +1,14 @@
 import vtk
 import tifffile
 
-
-def main():
-filename = "D:/SparesAMx_V06/Hussein_Versuch/Ti64_Hussein_complete/Hussein/Hussein_800_90.tif"
+filename = "filepath/filename.tif"
 
 colors = vtk.vtkNamedColors()
 
 # This is a simple volume rendering 
-# uses a vtkFixedPointVolumeRayCastMapper
-
 # Create the standard renderer, render window
 # and interactor.
 ren1 = vtk.vtkRenderer()
-
 renWin = vtk.vtkRenderWindow()
 renWin.AddRenderer(ren1)
 
@@ -26,6 +21,7 @@ reader.SetFileName(filename)
 reader.SetDataSpacing(1,1,0.3)
 
 # Create transfer mapping scalar value to opacity.
+# Values from slicer app
 opacityTransferFunction = vtk.vtkPiecewiseFunction()
 opacityTransferFunction.AddPoint(0, 0)
 opacityTransferFunction.AddPoint(20, 0)
@@ -34,6 +30,7 @@ opacityTransferFunction.AddPoint(98.26, 0.71)
 opacityTransferFunction.AddPoint(253, 0)
 
 # Create transfer mapping scalar value to color.
+# How to get Image color for 3d ?
 colorTransferFunction = vtk.vtkColorTransferFunction()
 colorTransferFunction.AddRGBPoint(0.0, 0, 0, 0)
 colorTransferFunction.AddRGBPoint(64.0, 1.0, 0.0, 0.0)
@@ -74,8 +71,9 @@ ren1.GetActiveCamera().Azimuth(45)
 ren1.GetActiveCamera().Elevation(30)
 ren1.ResetCameraClippingRange()
 ren1.ResetCamera()
+
 '''
-###test box widget to cut through planes###
+###test box widget to cut through planes### just for testing
 widget = vtk.vtkBoxWidget()
 widget.SetInteractor(iren)
 widget.SetPlaceFactor(1.25)
@@ -93,95 +91,9 @@ widget.AddObserver("EndInteractionEvent", SelectPolygons)
 widget.PlaceWidget()
 #############
 '''
-renWin.SetSize(600, 600)
-renWin.Render()
-
-iren.Start()
-
-
-def get_program_parameters():
-    import argparse
-    description = 'Volume rendering of a high potential iron protein.'
-    epilogue = '''
-    This is a simple volume rendering example that uses a vtkFixedPointVolumeRayCastMapper.
-    '''
-    parser = argparse.ArgumentParser(description=description, epilog=epilogue,
-                                     formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('filename', help='ironProt.vtk.')
-    args = parser.parse_args()
-    return args.filename
-
-
-if __name__ == '__main__':
-    main()
-
-
-
-
-
-# Create the reader for the Tiff-Data (Multi-Image-Tiff)
-reader = vtk.vtkTIFFReader()
-reader.SetFileName(filename)
-reader.SetDataSpacing(1,1,0.3)
-reader.Update()
-
-colors = vtk.vtkNamedColors()
-colors.SetColor("BkgColor", [51, 77, 102, 255])
-volumeMapper = vtk.vtkGPUVolumeRayCastMapper()
-volumeMapper.SetInputConnection(reader.GetOutputPort())
-
-volumeColor = vtk.vtkColorTransferFunction()
-volumeColor.AddRGBPoint(0.0, 0, 0, 0)
-volumeColor.AddRGBPoint(64.0, 1.0, 0.0, 0.0)
-volumeColor.AddRGBPoint(128.0, 0.0, 0.0, 1.0)
-volumeColor.AddRGBPoint(192.0, 0.0, 1.0, 0.0)
-volumeColor.AddRGBPoint(255.0, 0.1,0.1, 0.1)
-
-volumeScalarOpacity = vtk.vtkPiecewiseFunction()
-volumeScalarOpacity.AddPoint(0, 0.00)
-volumeScalarOpacity.AddPoint(50, 1)
-volumeScalarOpacity.AddPoint(255, 0)
-
-
-volumeGradientOpacity = vtk.vtkPiecewiseFunction()
-volumeGradientOpacity.AddPoint(10, 0.0)
-volumeGradientOpacity.AddPoint(100, 0.5)
-volumeGradientOpacity.AddPoint(200, 1.0)
-
-
-
-volumeProperty = vtk.vtkVolumeProperty()
-volumeProperty.SetColor(volumeColor)
-volumeProperty.SetScalarOpacity(volumeScalarOpacity)
-
-volumeProperty.SetInterpolationTypeToLinear()
-volumeProperty.ShadeOn()
-volumeProperty.SetAmbient(0.4)
-volumeProperty.SetDiffuse(0.6)
-volumeProperty.SetSpecular(0.2)
-
-volume = vtk.vtkVolume()
-volume.SetMapper(volumeMapper)
-volume.SetProperty(volumeProperty)
 
 renWin.SetSize(600, 600)
 renWin.Render()
 
 iren.Start()
-ren.AddViewProp(volume)
 
-
-
-
-
-
-
-
-
-luminance = vtk.vtkImageLuminance()
-luminance.SetInputConnection(colorVolume.GetImageDataConnection())
-append=vtk.vtkImageAppendComponents()
-append.AddInputConnection(colorVolume.GetImageDataConnection())
-append.AddInputConnection(luminance.GetOutputPort())
-append.Update()
-colorVolume.SetAndObserveImageData(append.GetOutput())
